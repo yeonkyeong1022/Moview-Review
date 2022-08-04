c98ae9d9898a4637d8339888e0d84a5f35e7ce77
yeonkyeong1022 <kimi0704@itc.ulsan.ac.kr>
2022-08-04 15:40:46 +0900

    ..

---
+++ src/main/java/org/zerock/mrebview/MrebviewApplication.java
@@ -0,0 +1,13 @@
+package org.zerock.mrebview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MrebviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MrebviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/zerock/mrebview/MrebviewApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zerock.mrebview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MrebviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
