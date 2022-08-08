9956a521aee75b0ec7d1f7ed7841f9f1daafdab1
yeonkyeong1022 <kimi0704@itc.ulsan.ac.kr>
2022-08-09 00:29:54 +0900

    read movie page

--- src/main/java/org/zerock/mreview/service/MovieService.java
+++ src/main/java/org/zerock/mreview/service/MovieService.java
@@ -17,6 +17,8 @@ public interface MovieService {
     Long register(MovieDTO movieDTO);
     PageResultDTO<MovieDTO, Object[]> getList(PageRequestDTO requestDTO);
 
+    MovieDTO getMovie(Long mno);
+
     default MovieDTO entitiesToDTO(Movie movie, List<MovieImage> movieImages, Double avg, Long reviewCnt){
         MovieDTO movieDTO = MovieDTO.builder()
                 .mno(movie.getMno())
